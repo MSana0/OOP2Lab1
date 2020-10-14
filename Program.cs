1308512898fea11028fdccff7f4d9fa5dc23d744
Marium Sana <mariumsana55@gmail.com>
2020-10-14 13:24:16 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Marium Sana Mim");
+            int x= 9;
+            int y = 81;
+            int sum = x + y;
+            int div = y / x;
+            Console.WriteLine("Sum:" + sum);
+            Console.WriteLine("Division:" + div);
+
+        }
+    }
+}
